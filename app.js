3f5c968c805356dbfe2147a42e3a7bf6dd17fca6
shasidharjavvaji <javvajishasidhar@gmail.com>
2016-09-21 14:13:50 +0530

    Session 5 assignment 2

---
+++ app.js
@@ -0,0 +1,6 @@
+var name = "shasidhar";
+var age = 22;
+var pob = "Vijayawada";
+var dob = new Date("Apr 24 1994");
+dob = dob.toDateString();
+console.log ("Name:" + name + "\nage: " + age + "\nPOB: " + pob + "\nDOB: " +dob);
